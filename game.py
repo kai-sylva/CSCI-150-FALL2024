35e223f96f958ca5cc4a0b9cacaf2a828f3eb8d3
Kai <iisylva.99@gmail.com>
2024-10-15 17:40:46 -0600

    Previous commit of game.py was incomplete; this is the complete version. Also added if statement to the transaction section if user attempts to purchase more apples than they have money for.

--- game.py
+++ game.py
@@ -1,3 +1,8 @@
+#Kai Rebich - game.py
+#10/15/2024 - CSCI 150
+#This file is a simulation of a simple game using the
+#functions imported from gamefunctions.py
+
 import gamefunctions
 
 user_name = input("What is your name? ")
@@ -11,5 +16,13 @@ quantityToPurchase = int(input("How many apples would you like to purchase? "))
 
 transaction = gamefunctions.purchase_item(0.99, startingMoney, quantityToPurchase)
 
-print(f'Awesome, here are your {transaction[0]} apples and your change of ${transaction[1]:.2f}!')
+if len(transaction) > len(range(0,2)):
+    print(f"\nSorry, you can only afford {transaction[0]} apples. Here they are and your change of ${transaction[1]:.2f}")
+else:
+    print(f'\nAwesome, here are your {transaction[0]} apples and your change of ${transaction[1]:.2f}!')
 
+monster = gamefunctions.new_random_monster()
+
+print(f"\nHave a good day and watch out for the {monster['name']} on your way home!")
+print(f"(You've encountered the {monster['name']} on your walk home. It is carrying ${monster['money']} and has {monster['health']} health points with a power rating of {monster['power']}.)")
+print("(Choose your next action wisely)")
